bfd2e6f37ae0b0d775bb7aa6c00f28025df5bc2f
Keila <keila.santiago.cics@ust.edu.ph>
2023-12-11 12:35:06 +0800

    button sa log in okey na validation

--- app/src/main/java/com/example/ics26011finalproject/SignInActivity.kt
+++ app/src/main/java/com/example/ics26011finalproject/SignInActivity.kt
@@ -21,75 +21,15 @@ class SignInActivity : AppCompatActivity() {
         val btnLogIn = findViewById<Button>(R.id.btnLogIn)
         val tvRegister = findViewById<TextView>(R.id.tvRegister)
 
-        /*btnLogIn.setOnClickListener {
-            val usernameEditText = findViewById<EditText>(R.id.username)
-            val passwordEditText = findViewById<EditText>(R.id.password)
-
-            val username = usernameEditText.text.toString()
-            val password = passwordEditText.text.toString()
-
-            if (username.trim().isNotEmpty() && password.trim().isNotEmpty()) {
-
-                val success = dbHandler.loginUser(username, password)
-
-                if (success) {
-                    val user = dbHandler.getUserInfo(username)
-
-                    if (user != null) {
-                        Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
-
-                        val userFragment = UserFragment()
-                        val bundle = Bundle()
-                        bundle.putString("USERNAME", user?.username)
-                        bundle.putString("FIRST_NAME", user?.firstName)
-                        bundle.putString("LAST_NAME", user?.lastName)
-                        bundle.putString("EMAIL", user?.email)
-                        userFragment.arguments = bundle
-
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frame_main, userFragment)
-                            .addToBackStack(null)
-                            .commit()
-
-                        usernameEditText.text.clear()
-                        passwordEditText.text.clear()
-
-                        finish()
-                    } else {
-                        Toast.makeText(this, "User information not found", Toast.LENGTH_LONG).show()
-                    }
-                } else {
-                    Toast.makeText(
-                        this,
-                        "Login Unsuccessful. Check your credentials.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            } else {
-                Toast.makeText(this, "Username or Password cannot be blank", Toast.LENGTH_LONG)
-                    .show()
-            }
-        }*/
-
-        tvRegister.setOnClickListener {
-
-            val i = Intent(this, RegisterActivity::class.java)
-            startActivity(i)
-
-        }
-
         btnLogIn.setOnClickListener {
 
-            val i = Intent(this, NavbarActivity::class.java)
-            startActivity(i)
-
             val usernameEditText = findViewById<EditText>(R.id.username)
             val passwordEditText = findViewById<EditText>(R.id.password)
 
-            val username = usernameEditText.text.toString()
-            val password = passwordEditText.text.toString()
+            val username = usernameEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
 
-            if (username.trim().isNotEmpty() && password.trim().isNotEmpty()) {
+            if (username.isNotEmpty() && password.isNotEmpty()) {
 
                 val success = dbHandler.loginUser(username, password)
 
@@ -99,6 +39,7 @@ class SignInActivity : AppCompatActivity() {
                     if (user != null) {
                         Toast.makeText(this, "Login Successful", Toast.LENGTH_LONG).show()
 
+                        // Move the intent creation and start here
                         val i = Intent(this, NavbarActivity::class.java)
                         i.putExtra("USERNAME", user.username)
                         i.putExtra("FIRST_NAME", user.firstName)
@@ -124,8 +65,13 @@ class SignInActivity : AppCompatActivity() {
                 Toast.makeText(this, "Username or Password cannot be blank", Toast.LENGTH_LONG)
                     .show()
             }
+        }
+
+        tvRegister.setOnClickListener {
+
+            val i = Intent(this, RegisterActivity::class.java)
+            startActivity(i)
 
         }
     }
 }
-
